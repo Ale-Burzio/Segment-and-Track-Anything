dae789ab2df44deeb05f3685ed8ad14ad7931620
Ale-Burzio <alessandro@ememe.ai>
2024-03-13 07:25:12 +0100

    mod installation script

--- script/download_ckpt.sh
+++ script/download_ckpt.sh
@@ -2,7 +2,7 @@
 gdown --id '1QoChMkTVxdYZ_eBlZhK2acq9KMQZccPJ' --output ./ckpt/R50_DeAOTL_PRE_YTB_DAV.pth
 
 # download sam-ckpt
-wget -P ./ckpt https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
+gdown --id '1qobFYrI4eyIANfBSmYcGuWRaSIXfMOQ8' --output ./ckpt/sam_hq_vit_h.pth
 
 # download grounding-dino ckpt
 wget -P ./ckpt https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
